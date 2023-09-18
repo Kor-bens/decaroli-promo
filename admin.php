9184cc5091f59e4133518fa85cdc08d36d710db9
Tortosa <sofianetortosa@gmail.com>
2023-09-18 17:03:00 +0200

    mise en place

---
+++ admin.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>admin-DECAROLI</title>
+    <link rel="stylesheet" href="admin.css">
+</head>
+<body>
+  <div id="navbar">
+    <ul>
+        <li><a href="index.html">Page promo</a></li>
+        <li><a href="modif-promo">Modifier la page</a></li>
+        
+    </ul>
+  </div>
+    
+</body>
+</html>
